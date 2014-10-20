67ef7108fa9d06ca673d71dd6ea760e3d637d645
Alexey Malov <alexey.malov@cpslabs.net>
2014-10-21 02:16:50 +0400

    any_of algorithm sample was added

--- algorithms/find_if(boost)/main.cpp
+++ algorithms/find_if(boost)/main.cpp
@@ -6,13 +6,6 @@
 #include <boost/range/adaptor/filtered.hpp>
 #include <boost/phoenix.hpp>
 
-
-#include <random>
-#include <boost/range/algorithm/generate.hpp>
-#include <boost/range/algorithm/count_if.hpp>
-#include <boost/range/algorithm/count.hpp>
-#include <boost/timer/timer.hpp>
-
 using namespace std;
 using namespace boost::range;
 using namespace boost::adaptors;
@@ -43,64 +36,8 @@ void FindLastNegativeNumberInVector()
 	}
 }
 
-void Benchmark(function<size_t()> const& fn)
-{
-	size_t num = 0;
-	{
-		boost::timer::auto_cpu_timer t;
-		for (int i = 0; i < 500; ++i)
-		{
-			num += fn();
-		}
-	}
-	cout << num << endl;
-}
-
 void main()
 {
 	FindFirstEvenValueInArray();
-	FindLastNegativeNumberInVector();	
-
-	mt19937 gen;
-	vector<int> randomNumbers;
-	randomNumbers.reserve(1000000);
-	generate_n(back_inserter(randomNumbers), 1000000, gen);
-
-	size_t const size = randomNumbers.size();
-
-	Benchmark([&randomNumbers, size](){
-		size_t numNegativeNumbers = 0;
-		for (size_t i = 0; i < size; ++i)
-		{
-			if (randomNumbers[i] < 0)
-			{
-				++numNegativeNumbers;
-			}
-		}
-		return numNegativeNumbers;
-	});
-
-	Benchmark([&randomNumbers](){
-		size_t numNegativeNumbers = 0;
-		for (auto value : randomNumbers)
-		{
-			if (value < 0)
-			{
-				++numNegativeNumbers;
-			}
-		}
-		return numNegativeNumbers;
-	});
-
-	Benchmark([&randomNumbers](){
-		return count_if(randomNumbers.begin(), randomNumbers.end(), [](int value){return value < 0; });
-	});
-
-	Benchmark([&randomNumbers](){
-		return count_if(randomNumbers, [](int value){return value < 0; });
-	});
-
-	Benchmark([&randomNumbers](){
-		return count_if(randomNumbers, arg1 < 0);
-	});
+	FindLastNegativeNumberInVector();
 }
