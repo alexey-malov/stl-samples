dd6afe186e8acfdcb545f279da0dcd9b2c0b4be4
Alexey Malov <alexey.malov@cpslabs.net>
2015-01-20 18:51:31 +0300

    Добавлены примеры алгоритмов transform и использования итератора transform_iterator

---
+++ algorithms/transform/main.cpp
@@ -0,0 +1,26 @@
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+void IntsToStrings()
+{
+	vector<int> ints = {1, 2, -3, 42, 13};
+
+	vector<string> strings;
+	strings.reserve(ints.size());
+
+	transform(ints.begin(), ints.end(), back_inserter(strings), [](int number){
+		return "{" + to_string(number) + "}";
+	});
+
+	copy(strings.begin(), strings.end(), ostream_iterator<string>(cout, ", "));
+	cout << endl;
+}
+
+void main()
+{
+	IntsToStrings();
+}
